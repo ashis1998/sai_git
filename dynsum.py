9c3a7d128595b1c8a9f94c36934f17cecf4ffa64
unknown <ashischoudhury1998@gmail.com>
2022-02-14 10:05:09 +0530

    i created added a new file

---
+++ dynsum.py
@@ -0,0 +1,11 @@
+#dynsum.py
+print("Enter two values:")
+x3=float(input())+float(input())
+print("-------------")
+print("Sum={}".format(x3))
+print("-*50")
+print("Enter two values:")
+x3=float(input())*float(input())
+print("-------------")
+print("Mul={}".format(x3))
+
